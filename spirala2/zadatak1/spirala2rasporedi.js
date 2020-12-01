75bee8def1042b3fb55ce8f0984371b7272ad0d3
Ajla Dzaferbegovic <56268385+adzaferbeg1@users.noreply.github.com>
2020-12-01 16:18:21 +0100

    uradjena druga funkcija sa svim provjerama

--- spirala2/zadatak1/spirala2rasporedi.js
+++ spirala2/zadatak1/spirala2rasporedi.js
@@ -6,7 +6,7 @@ var okvir4=document.getElementById("okvir4");
 
 function pozoviIscrtajRaspored(){
 iscrtajRaspored(okvir,['Ponedjeljak','Utorak','Srijeda','Četvrtak','Petak'],8,21);
-iscrtajRaspored(okvir2,['Ponedjeljak','Utorak','Srijeda'],4,15);
+iscrtajRaspored(okvir2,['Ponedjeljak','Utorak','Srijeda'],16,22);
 iscrtajRaspored(okvir3,['Ponedjeljak','Utorak','Srijeda'],15,4);
 iscrtajRaspored(okvir4,['Ponedjeljak','Utorak','Srijeda'],8.5,21);
 dodajAktivnost(okvir,"WT","predavanje",9,12,"Ponedjeljak");
@@ -16,18 +16,6 @@ dodajAktivnost(okvir,"RMA","vježbe",12.5,14,"Utorak");
 dodajAktivnost(okvir,"DM","tutorijal",14,16,"Utorak");
 dodajAktivnost(okvir,"DM","predavanje",16,19,"Utorak");
 dodajAktivnost(okvir,"OI","predavanje",12,15,"Srijeda");
-
-
-
-/* --okvir koji nije kreiran
-
-dodajAktivnost(document.getElementById("okvir5"),"WT","vježbe",12.8,13,"Utorak");
-*/
-/* --unos aktivnosti da pogresnim vremenom pocetka i pogresnim vremenom kraja
-
-dodajAktivnost(document.getElementById("okvir4"),"WT","vježbe",12.8,13,"Utorak");
-dodajAktivnost(document.getElementById("okvir4"),"WT","vježbe",12.5,13.7,"Utorak");
-*/
-
+dodajAktivnost(okvir2,"OI","predavanje",16.5,18,"Srijeda");
 }
 
