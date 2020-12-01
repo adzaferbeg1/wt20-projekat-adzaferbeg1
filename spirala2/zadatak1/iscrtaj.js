55045a8e722fb6dd2517ff144e2081ddd4b24cba
Ajla Dzaferbegovic <56268385+adzaferbeg1@users.noreply.github.com>
2020-12-01 01:14:13 +0100

    uradjena druga funkcija

--- spirala2/zadatak1/iscrtaj.js
+++ spirala2/zadatak1/iscrtaj.js
@@ -1,6 +1,11 @@
 /* prva funkcija */
 
+var pocetakRasporeda;
+var krajRasporeda;
+
 function iscrtajRaspored(div, dani, satPocetak, satKraj){
+    pocetakRasporeda = satPocetak;
+    krajRasporeda = satKraj;
 
     if(!Number.isInteger(satPocetak) || !Number.isInteger(satKraj)){
     div.innerHTML = "Greška"; 
@@ -26,36 +31,93 @@ function iscrtajRaspored(div, dani, satPocetak, satKraj){
                 }else th.innerHTML= pocetniSat + ":00";
               
                 tr.appendChild(th);
-                pocetniSat++;
-              
-              
+                pocetniSat++;    
         }
-          
           tbdy.appendChild(tr);
-
       }else{
             var tr = document.createElement('tr');
-        for(var j=0; j<27; j++){
+        for(var j=0; j<(satKraj-satPocetak+0.5)*2; j++){
             var td = document.createElement('td');
+            if(j%2!=0) td.className = "paran";
             if(j==0){
                 td.className = "dan";
-                td.innerHTML = dani[i-1];
+                td.innerHTML = dani[i-1]; 
             } 
             tr.appendChild(td);
+            tr.id=dani[i-1];
         }
         tbdy.appendChild(tr);
     }
-    
     }
     tbl.appendChild(tbdy);
     div.appendChild(tbl)
 }
-
 }
 
 /* druga funkcija */
-
+    var termini = new Array();
 
 function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj, dan){
+    
+    if(raspored==null){
+        alert("Greška - raspored nije kreiran.");
+        return false;
+    }else{ 
+    if(vrijemeKraj<vrijemePocetak){
+      alert("Greška - u rasporedu ne postoji dan ili vrijeme u kojem pokušavate dodati termin");
+      //return false;
+    }else{
+        
+        if(!Number.isInteger(vrijemePocetak)){
+            if(vrijemePocetak != Number.parseInt(vrijemePocetak)+0.5) alert("Greška - u rasporedu ne postoji dan ili vrijeme u kojem pokušavate dodati termin");
+            //return false;
+        }
+        if(!Number.isInteger(vrijemeKraj)){
+            if(vrijemeKraj != Number.parseInt(vrijemeKraj)+0.5) alert("Greška - u rasporedu ne postoji dan ili vrijeme u kojem pokušavate dodati termin");
+            //return false;
+        }
+        if(termini.includes(vrijemePocetak) || termini.includes(vrijemeKraj)){
+            alert("Greška - već postoji termin u rasporedu u zadanom vremenu");
+           // return false;
+        }else{ 
+            var mapa = new Map();
+            mapa.set("Ponedjeljak", 1);
+            mapa.set("Utorak", 2);
+            mapa.set("Srijeda", 3);
+            mapa.set("Četvrtak", 4);
+            mapa.set("Petak", 5);
 
+            var red = raspored.getElementsByTagName("tr") [mapa.get(dan)];
+           
+           //trazenje celije za ubaciti 
+            var trazeni = 0;
+            var trazenaCelija = 0;
+            var ukupanBrojCelijePrije = red.cells.length;
+            for(var k=0; k<ukupanBrojCelijePrije; k++){
+                if(red.getElementsByTagName("td")[k].colSpan == undefined) {trazeni += 1; trazenaCelija++;}
+                else {trazeni += red.getElementsByTagName("td")[k].colSpan; trazenaCelija++; }
+                if(trazeni == (vrijemePocetak-pocetakRasporeda)*2 + 2 ) break;
+            }
+            console.log(trazeni);
+            
+
+            var celija = red.getElementsByTagName("td")[trazenaCelija];
+            celija.className = "predmet";
+            celija.innerHTML = naziv + "<br>" + tip;
+            celija.colSpan = (vrijemeKraj-vrijemePocetak)*2;
+            
+            //brisanje viska celija
+            for(let p = 1; p<(vrijemeKraj-vrijemePocetak)*2; p++){
+                red.deleteCell(trazenaCelija+1);
+            }
+            
+           
+          
+       
+        }
+        
+    
 }
+}
+}
+
--- spirala2/zadatak1/spirala2rasporedi.js
+++ spirala2/zadatak1/spirala2rasporedi.js
@@ -9,6 +9,25 @@ iscrtajRaspored(okvir,['Ponedjeljak','Utorak','Srijeda','Četvrtak','Petak'],8,2
 iscrtajRaspored(okvir2,['Ponedjeljak','Utorak','Srijeda'],4,15);
 iscrtajRaspored(okvir3,['Ponedjeljak','Utorak','Srijeda'],15,4);
 iscrtajRaspored(okvir4,['Ponedjeljak','Utorak','Srijeda'],8.5,21);
-dodajAktivnost(okvir4,"WT","vježbe",12.8,13,"Utorak");
+dodajAktivnost(okvir,"WT","predavanje",9,12,"Ponedjeljak");
+dodajAktivnost(okvir,"WT","vježbe",12,13.5,"Ponedjeljak");
+dodajAktivnost(okvir,"RMA","predavanje",14,17,"Ponedjeljak");
+dodajAktivnost(okvir,"RMA","vježbe",12.5,14,"Utorak");
+dodajAktivnost(okvir,"DM","tutorijal",14,16,"Utorak");
+dodajAktivnost(okvir,"DM","predavanje",16,19,"Utorak");
+dodajAktivnost(okvir,"OI","predavanje",12,15,"Srijeda");
+
+
+
+/* --okvir koji nije kreiran
+
+dodajAktivnost(document.getElementById("okvir5"),"WT","vježbe",12.8,13,"Utorak");
+*/
+/* --unos aktivnosti da pogresnim vremenom pocetka i pogresnim vremenom kraja
+
+dodajAktivnost(document.getElementById("okvir4"),"WT","vježbe",12.8,13,"Utorak");
+dodajAktivnost(document.getElementById("okvir4"),"WT","vježbe",12.5,13.7,"Utorak");
+*/
+
 }
 
